8cf7096a3e30f81e925c39a0a1f8d79a3e61f1be
sol4nki <solankipranjal44@gmail.com>
2026-01-01 23:05:47 +0530

    py img folder

--- init.py
+++ init.py
@@ -8,11 +8,17 @@ import os
 #     except FileExistsError:
 #         print(f"'PHASE-{i}' already created.")
 
+# for i in range(9):
+#     file_path = f"./PHASE-{i}/README.md"
+#     with open(file_path, "w") as file:
+#         file.write(f" ")
+#         print(f"Created {file_path}")
+
+
 for i in range(9):
-    file_path = f"./PHASE-{i}/README.md"
-    with open(file_path, "w") as file:
-        file.write(f" ")
-        print(f"Created {file_path}")
-
-
-
+    path = f"./PHASE-{i}/img"
+    try:
+        os.mkdir(path)
+        print(f"'PHASE-{i}/img' created.")
+    except FileExistsError:
+        print(f"'PHASE-{i}/img' already created.")
